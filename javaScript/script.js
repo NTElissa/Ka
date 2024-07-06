8622a44523614275b4928ed3ed2aaaf6b926c577
NTElissa <122989293+NTElissa@users.noreply.github.com>
2024-07-06 21:54:17 +0200

    new update

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,25 +1,10 @@
-const toggle = () => {
-    const nav = document.getElementById("topnav");
-    nav.className === "topnav" ? nav.className += " responsive" : nav.className = "topnav";
-};
-
-document.addEventListener('DOMContentLoaded', () => {
-    const photos = document.querySelectorAll('.photo-preview img');
-    const overlay = document.createElement('div');
-    const fullSizeImage = document.createElement('img');
-    
-    overlay.className = 'full-size-overlay';
-    overlay.appendChild(fullSizeImage);
-    document.body.appendChild(overlay);
-    
-    photos.forEach(photo => {
-        photo.addEventListener('click', () => {
-            fullSizeImage.src = photo.src;
-            overlay.style.display = 'flex';
-        });
-    });
-    
-    overlay.addEventListener('click', () => {
-        overlay.style.display = 'none';
-    });
-});
+function toggleNav() {
+    var nav = document.getElementById("topnav");
+    var navLinks = document.getElementById("nav-links");
+    if (nav.className === "topnav") {
+        nav.className += " responsive";
+    } else {
+        nav.className = "topnav";
+    }
+    navLinks.style.display = (navLinks.style.display === "block") ? "none" : "block";
+}
